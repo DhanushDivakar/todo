458d99675eba8b1b49ed57ee59ff94831e718962
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-11-25 19:39:10 +0530

    bloc

---
+++ lib/cubit/counter_state.dart
@@ -0,0 +1,10 @@
+part of 'counter_cubit.dart';
+
+
+//!this is the blueprint for all the states emitted by the counterstate
+class CounterState {
+  int counterValue;
+  CounterState({required this.counterValue});
+
+
+}
