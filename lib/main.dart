989a1380e9ded7ae7b48cd612140a65e9f20e75e
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-11-25 19:18:55 +0530

    hiii

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-//
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
        
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter'),
+      home: const MyHomePage(title: 'Counter app'),
     );
   }
 }
