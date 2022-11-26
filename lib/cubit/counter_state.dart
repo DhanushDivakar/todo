b9be3ac9a576bd9f36c2be3557c782a31e599bd4
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-11-26 18:27:54 +0530

    first

--- lib/cubit/counter_state.dart
+++ lib/cubit/counter_state.dart
@@ -1,8 +1,9 @@
 part of 'counter_cubit.dart';
 
 
+
 //!this is the blueprint for all the states emitted by the counterstate
-class CounterState {
+class CounterState  {
   int counterValue;
   bool wasIncremented;
   CounterState({required this.counterValue, required this.wasIncremented});
