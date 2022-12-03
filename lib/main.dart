f347063c0b0ae45416dc451b32bf565bf99f90ad
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-12-03 12:15:24 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:todo/cubit/counter_cubit.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
-  //! This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<CounterCubit>(
-      create: (context) => CounterCubit(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const MyHomePage(),
-      ),
-    );
-  }
-}
-
-//hiiii
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text("Counter App"),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          // ignore: prefer_const_literals_to_create_immutables
-          children: [
-            const Text(" You have pushed the button this many times "),
-            const SizedBox(
-              height: 20,
-            ),
-            BlocConsumer<CounterCubit, CounterState>(
-              listener: (context, state) {
-            if (state.wasIncremented == true) {
-          Scaffold.of(context).showSnackBar(const SnackBar(
-            content: Text("Incremented"),
-            duration: Duration(milliseconds: 300),
-          ),);
-        }
-        else if(state.wasIncremented == false){
-           Scaffold.of(context).showSnackBar(const SnackBar(
-            content: Text("Decremented"),
-            duration: Duration(milliseconds: 300),
-          ),);
-        }
-
-        
-              
-             
-              },
-               builder: (context, state) {
-                return Text(
-                  state.counterValue.toString(),
-                  style: const TextStyle(
-                      fontSize: 40, fontWeight: FontWeight.w500),
-                );
-              },
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).decrement();
-                  },
-                  tooltip: 'Decrement',
-                  child: const Icon(Icons.remove),
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).increment();
-                  },
-                  tooltip: 'Increment',
-                  child: const Icon(Icons.add),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
+    return Container();
   }
 }
